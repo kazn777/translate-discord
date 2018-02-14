4921d272653d7fda58edfa088f78d9b2add6a64e
wilsonaar <ajw43@buffalo.edu>
2018-02-14 14:31:58 -0500

    Added localized list of languages so bot does not need to call it every time. Bot can now translate to any language if you run the command <.translate arg> where arg is any language code or language

--- translate-discord.py
+++ translate-discord.py
@@ -1,6 +1,6 @@
 import discord
 from google.cloud import translate
-
+import json
 discord_client = discord.Client()
 
 keyfile = open("keys.txt", "r")
@@ -17,7 +17,6 @@ async def on_ready():
     print(discord_client.user.name)
     print(discord_client.user.id)
     print("----------------------------")
-
     await discord_client.change_presence(game=discord.Game(name=".translate help"))
     print("--- Bot Presence Changed ---")
     print("")
@@ -34,8 +33,19 @@ async def on_message(message):
             # When the user wants to call for help, or translate to more than english, this will parse their intention
             args = content.split(" ", 1)[1]
             print(args)
+            langcode = check_language(args)
             if args.lower() == "help":
-                await discord_client.send_message(message.channel, "Help message TODO")
+                help_message = "Type **.translate** to translate the message above to english.\n" \
+                               " Type **.translate <language>** to translate it to that language."
+                await discord_client.send_message(message.channel, help_message)
+            elif langcode != "False":
+                translation = translate_message(last_message, langcode)
+                formatted_return = ("```" +
+                                    "Translation: " + translation['translatedText'] + "\n"
+                                    "Source Language : " + translation['detectedSourceLanguage'] +
+                                    "```")
+                await discord_client.send_message(message.channel, formatted_return)
+
         else:
             translation = translate_message(last_message, "en")
             formatted_return = ("```" +
@@ -48,11 +58,24 @@ async def on_message(message):
     last_message = content
 
 
+# Translates the message and returns the output object.
 def translate_message(message, target):
     translate_client = translate.Client.from_service_account_json('api-key.json')
     result = translate_client.translate(message, target_language=target)
     print(u'Text: {}'.format(result['input']))
-    return result;
+    return result
+
+
+# Takes in a string that may or may not be a language code.
+# If it is a language code, it returns said code.
+# If it is a language, it returns the affiliated code. If not, it returns -1
+def check_language(code):
+    with open('languages.json') as data:
+        languages = json.load(data)
+        for lang in languages:
+            if (lang['language'].lower() == code.lower())or (lang['name'].lower() == code.lower()):
+                return lang['language']
+        return "False"
 
 
 discord_client.run(key)
